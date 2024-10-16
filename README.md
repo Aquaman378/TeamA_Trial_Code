b3aa6f95c74387b8461ff8a6d7bc54014df045eb
Jalon Harris <jalonalexh37@gmail.com>
2024-10-15 22:18:24 -1000

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# 1516X - High Stakes
+
+This is the [High Stakes repository](https://github.com/calhighrobotics/high_stakes_x) for team **1516X**
+
+We are a VEX VRC team based in the Bay Area, California.
+
+We have both a wiki that covers high-level programming, engineering, design, and team management topics, as well as Doxygen documentation for our codebase.
+
+- [Wiki](https://calhighrobotics.github.io/1516x)
+- [Doxygen Documentation](https://calhighrobotics.github.io/high_stakes_x)
+
+We aim to have a cohesive codebase that covers many subsystems but also follows best practices in the C++ development space as well as bringing in new features through modularity.
+
+The codebase is split up into:
+
+```text
+/include - Contains information about the declarations of various methods and how they would be used
+   /robot - Contains the header files and information custom for the actual robot.
+/src - Contains implementation of the above methods and the definition, specific to the 1516X robot
+```
+
+## Other Libraries
+
+We use the following libraries to make our codebase possible. Check out their code for types and functions that we use as well.
+
+- [PROS](https://pros.cs.purdue.edu/v5/pros-4/)
+- [Lemlib](https://lemlib.readthedocs.io/)
+- [lvgl](https://docs.lvgl.io/8.3/)
+
+## License
+
+See [LICENSE](https://github.com/calhighrobotics/high_stakes_x/blob/main/LICENSE) for more information.
---
+++ src/globals.cpp
@@ -0,0 +1,101 @@
+#include "globals.h"
+#include "pros/abstract_motor.hpp"
+#include "pros/motors.hpp"
+
+/*
+ * Although the following constants belong in their own seperate
+ * files(auton.cpp, drivetriain.cpp), they are put here in order to maintain a
+ * common location for all of the constants used by the program to belong in.
+ * NOTE: This is the location where these variables are put into memory, but
+ * they can be otherwise modified throughout the program.
+ */
+
+// Defines the objects that are used by the program for each of the individual
+// subsystems.
+
+namespace Robot {
+namespace Globals {
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+pros::Motor RightFront(6, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::Motor LeftFront(-20, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::Motor LeftBack(-10, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::Motor RightBack(8, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::Motor IntakeMotor(-9, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+//pros::Motor HookMotor(4, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+
+//pros::adi::DigitalIn drivetrainToggleSwitch('G');
+//pros::adi::DigitalIn autonToggleSwitch('F');
+
+//pros::Optical colorSensor(4); // placeholder port number
+
+pros::adi::Pneumatics LatchControl('A', false);
+pros::adi::Pneumatics IntakeToggle('B', false);
+
+//pros::Imu inertial_sensor(15);
+
+pros::MotorGroup drive_left({LeftFront.get_port(), LeftBack.get_port()});
+pros::MotorGroup drive_right({RightFront.get_port(), RightBack.get_port()});
+pros::MotorGroup drive_({LeftFront.get_port(), RightFront.get_port(),
+                         LeftBack.get_port(), RightBack.get_port()});
+
+// Describes the lemlib objects that are used to control the autonomous
+// functions of the robot.
+lemlib::Drivetrain drivetrain{
+    &drive_left,  // left drivetrain motors
+    &drive_right, // right drivetrain motors
+    12.25,       // track width
+    lemlib::Omniwheel::OLD_325,
+    450, // drivetrain rpm is 450
+    2    // horizontal drift is 2
+};
+
+lemlib::OdomSensors sensors{
+    nullptr,         // vertical tracking wheel 1
+    nullptr,         // vertical tracking wheel 2
+    nullptr,         // horizontal tracking wheel 1
+    nullptr,         // we don't have a second tracking wheel, so we set it to nullptr
+    &inertial_sensor // inertial sensor
+};
+
+// forward/backward PID
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+lemlib::ExpoDriveCurve throttle_curve(3, // joystick deadband out of 127
+                                     10, // minimum output where drivetrain will move out of 127
+                                     1.019 // expo curve gain
+);
+
+// input curve for steer input during driver control
+lemlib::ExpoDriveCurve steer_curve(3, // joystick deadband out of 127
+                                  10, // minimum output where drivetrain will move out of 127
+                                  1.019 // expo curve gain
+);
+
+lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller, sensors, &throttle_curve, &steer_curve);
+
+} // namespace Globals
+
+} // namespace Robot
